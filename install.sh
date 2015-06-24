408aaa2fe050d85e8d307f3953863a0086706e24
William Falk-Wallace <wgf2104@columbia.edu>
2015-06-23 22:56:55 -0700

    begin remove netrc

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 backup=~/dotfiles_old
 # list of files/folders to symlink in homedir
 bash_files="bashrc bash_profile bash_aliases bash_functions bash_header"
-aux_files_2="gitconfig gitignore_global vimrc inputrc netrc ansiweatherrc"
+aux_files_2="gitconfig gitignore_global vimrc inputrc ansiweatherrc"
 aux_files_1="jrnl_config git-completion.bash assistant optimizelyrc"
 files="$bash_files $aux_files_1 $aux_files_2"
 # TODO: vim colors
