63b1a750fda998f69da078d3b6a4d7730e86ec45
William Falk-Wallace <wgf2104@columbia.edu>
2015-06-26 14:08:53 -0700

    more pretty debug log

--- install.sh
+++ install.sh
@@ -9,32 +9,39 @@
 # dotfiles directory
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # old dotfiles backup directory
-backup=~/dotfiles_old
+backup=dotfiles_old
 # list of files/folders to symlink in homedir
 bash_files="bashrc bash_profile bash_aliases bash_functions bash_header"
-aux_files_2="gitconfig gitignore_global vimrc inputrc ansiweatherrc"
-aux_files_1="jrnl_config git-completion.bash assistant optimizelyrc"
-files="$bash_files $aux_files_1 $aux_files_2"
-# TODO: vim colors
+git_files="gitconfig gitignore_global git-completion.bash"
+aux_files="vimrc inputrc optimizelyrc"
+files="$bash_files $git_files $aux_files"
+
 ###############################################################################
 
 # create dotfiles_old in homedir
 echo "Creating $backup for backup of any existing dotfiles in ~"
-mkdir -p $backup
+mkdir -p ~/$backup
 echo "...done"
 
 # move any existing dotfiles in dir to backup directory, then create symlinks
 for file in $files; do
   if [[ -L ~/.$file ]]; then
-    echo "-------------------------"
-    echo "rm -f ~/.$file -- SYMLINK"
+    echo ".$file is a symlink"
+    echo "removing .$file"
+    echo "rm -f ~/.$file"
+    echo
     rm -f ~/.$file
   else
-    echo "-------------------------"
+    echo ".$file is a file"
+    echo "moving .$file to $backup/$file"
     echo "mv ~/.$file $backup/$file"
+    echo
     mv ~/.$file $backup/$file
   fi
-  echo "-------------------------"
+  echo "symlinking .$file ~/.$file"
   echo "ln -s $dir/$file ~/.$file"
+  echo
   ln -s $dir/$file ~/.$file
 done
+
+echo "...done"
